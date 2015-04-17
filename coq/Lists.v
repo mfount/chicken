1d5bd1b5cdfd6690e68d51cdf57239ad9c6f33ae
Ben Wetherfield <bwetherfield@gmail.com>
2015-04-17 10:56:46 -0400

    with compiled files

--- coq/Lists.v
+++ coq/Lists.v
@@ -117,7 +117,7 @@ Proof.
 Theorem fst_swap_is_snd : forall (p : natprod),
   fst (swap_pair p) = snd p.
 Proof.
- intros p. destruct p as [n m]. simpl. reflexivity.
+ intros p. destruct p as [n m]. simpl. reflexivity. Qed.
 (** [] *)
 
 (* ###################################################### *)
