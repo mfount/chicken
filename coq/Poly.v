9a637a6462e4b24cfef078f3190566d6ca650447
ChannyClaus <chankang17@gmail.com>
2015-04-24 14:42:11 -0400

    filter_even_gt7 in poly.v

--- coq/Poly.v
+++ coq/Poly.v
@@ -7,7 +7,7 @@
     as data).
 *)
 
-Require Export Lists.   
+Require Export Lists.
 
 (* ###################################################### *)
 (** * Polymorphism *)
@@ -763,16 +763,17 @@ Proof. reflexivity.  Qed.
     and returns a list of just those that are even and greater than
     7. *)
 (* TODO BC *)
+
 Definition filter_even_gt7 (l : list nat) : list nat :=
-  (* FILL IN HERE *) admit.
+  filter (fun x => andb (evenb x) (ble_nat 7 x)) l.
 
 Example test_filter_even_gt7_1 :
   filter_even_gt7 [1;2;6;9;10;3;12;8] = [10;12;8].
- (* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
 
 Example test_filter_even_gt7_2 :
   filter_even_gt7 [5;2;6;19;129] = [].
- (* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
 (** [] *)
 
 (** **** Exercise: 3 stars (partition)  *)
