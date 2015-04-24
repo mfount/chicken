6741f5f28d38bc45d4677b10c167a52d65878e68
ChannyClaus <chankang17@gmail.com>
2015-04-24 10:30:02 -0400

    alternate Lists.v

--- coq/Lists.v
+++ coq/Lists.v
@@ -309,19 +309,22 @@ Example test_countoddmembers3:    countoddmembers nil = 0.
     both lists at the same time.  (One possible solution requires
     defining a new kind of pairs, but this is not the only way.)  *)
 
-
 Fixpoint alternate (l1 l2 : natlist) : natlist :=
-  (* FILL IN HERE *) admit.
+  match l1, l2 with
+  | [], _ => l2
+  | _, [] => l1
+  | h1 :: t1, h2 :: t2 => h1 :: h2 :: (alternate t1 t2)
+  end.
 
 
 Example test_alternate1:        alternate [1;2;3] [4;5;6] = [1;4;2;5;3;6].
- (* FILL IN HERE *) Admitted.
+  Proof. reflexivity. Qed.
 Example test_alternate2:        alternate [1] [4;5;6] = [1;4;5;6].
- (* FILL IN HERE *) Admitted.
+  Proof. reflexivity. Qed.
 Example test_alternate3:        alternate [1;2;3] [4] = [1;4;2;3].
- (* FILL IN HERE *) Admitted.
+  Proof. reflexivity. Qed.
 Example test_alternate4:        alternate [] [20;30] = [20;30].
- (* FILL IN HERE *) Admitted. 
+  Proof. reflexivity. Qed.
 (** [] *)
 
 (* ###################################################### *)
